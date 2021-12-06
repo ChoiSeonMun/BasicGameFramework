d7eea062be331ae16d24d2aa4a245b8cf4ee5c32
smchoi <seonmun.choi@gmail.com>
2021-12-06 19:10:36 +0900

    Remove some function of IBehaviour
    우선 기본적인 것부터 하고나서 구현하는 것으로.

--- BasicGameFramework/BasicGameFramework/IBehaviour.h
+++ BasicGameFramework/BasicGameFramework/IBehaviour.h
@@ -9,9 +9,6 @@ struct IBehaviour
 	virtual void		Init() { }
 	virtual void		Update() { }
 	virtual void		PhysicsUpdate() { }
-	virtual void		LateUpdate() { }
 	virtual void		Render(HDC hdc) { }
 	virtual void		Release() { }
-	virtual void		OnCollision() { }
-	virtual void		OnDebug(HDC hdc) { }
 };
